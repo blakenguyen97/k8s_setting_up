23d5d5b07bc74b1baafb527680ef37253b5e37d6
blake.nguyen97 <52020800+blakenguyen97@users.noreply.github.com>
2021-03-30 11:34:20 +0700

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,3 +1,4 @@
+# Don't forget to turn off your swap before running this file.
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 
 mkdir -p $HOME/.kube
@@ -7,8 +8,8 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # CNI Plugin
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-# NVIDIA GPU Plugin
-kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.$
+# NVIDIA GPU Plugin, uncomment it if you wish to run application using GPU
+#kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.$
 
 # Taint master node
 kubectl taint nodes --all node-role.kubernetes.io/master-
